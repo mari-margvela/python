e22c357534d5c848f20814593186d5f4456ecc57
mari-margvela <margvelashvili04@bk.ru>
2022-10-14 00:39:58 +0300

    sleep

---
+++ 1.py
@@ -0,0 +1,10 @@
+name = "Mari"
+surname = "Margvela"
+data = 2004
+print(name, surname, data)
+
+print(name, surname, data, sep="_")
+
+name, surname = surname, name
+data += 60
+print(name, surname, data)
